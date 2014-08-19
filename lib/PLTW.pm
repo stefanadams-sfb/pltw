2114a94a4c120357976137e699ad0267b4d088d2
Local Admin <local@local.local>
2014-08-19 09:09:07 -0500

    Add content

--- lib/PLTW.pm
+++ lib/PLTW.pm
@@ -16,8 +16,7 @@ sub startup {
   # Normal route to controller
   $r->get('/')->to('example#welcome');
   $r->get('/partners/:size')->to('partners#list', size => 2);
-  $r->get('/syllabus')->to('syllabus#index');
-  $r->get('/curriculum')->to('curriclum#index');
+  $r->get('/books')->to('books#index');
   $r->get('/communication')->to('communication#index');
   $r->get('/resources')->to('resources#index');
   $r->get('/articles')->to('articles#index');

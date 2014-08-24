78cfec4c0f0d5745919a3bc7af40ed1db1474847
Local Admin <local@local.local>
2014-08-24 18:50:11 -0500

    Add photo roster, remember partners

--- lib/PLTW.pm
+++ lib/PLTW.pm
@@ -16,8 +16,10 @@ sub startup {
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->get('/')->to('example#welcome');
-  $r->get('/partners/:size')->to('partners#list', size => 2);
+  #$r->get('/')->to('example#welcome');
+  $r->get('/')->to('example#index');
+  $r->get('/partners/:size')->to('partners#list', size => '');
+  $r->get('/roster')->to('roster#list');
   $r->get('/books')->to('books#index');
   $r->get('/communication')->to('communication#index');
   $r->get('/resources')->to('resources#index');
--- lib/PLTW/Controller/Partners.pm
+++ lib/PLTW/Controller/Partners.pm
@@ -1,4 +1,10 @@
 package PLTW::Controller::Partners;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Mojo::Redis2;
+
+my $redis = Mojo::Redis2->new;
+
+sub list { shift->render(redis => $redis) }
+
 1;

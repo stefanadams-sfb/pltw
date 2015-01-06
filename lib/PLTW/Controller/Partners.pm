3796f3cddb80c73b4196d2399e458e8c2cb53762
Stefan Adams <stefan@borgia.com>
2015-01-06 12:08:08 -0600

    fix partner wildcard algorithm

--- lib/PLTW/Controller/Partners.pm
+++ lib/PLTW/Controller/Partners.pm
@@ -5,6 +5,17 @@ use Mojo::Redis2;
 
 my $redis = Mojo::Redis2->new;
 
-sub list { shift->render(redis => $redis) }
+sub list {
+  my $self = shift;
+  my $xkcd = $self->config('xkcd');
+  my %xkcd = %$xkcd;
+  $xkcd = (keys %xkcd)[rand keys %xkcd];
+  my $moral = $xkcd{$xkcd};
+  $self->render_later;
+  $self->ua->get("http://xkcd.com/$xkcd/info.0.json" => sub {
+    my ($ua, $tx) = @_;
+    $self->render(redis => $redis, img => $tx->res->json->{img}, alt => $tx->res->json->{alt}, moral => $moral);
+  });
+}
 
 1;
